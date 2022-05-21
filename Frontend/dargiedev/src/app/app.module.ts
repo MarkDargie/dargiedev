d54818f85b0817cf2c08cb12e0fc2f9b81dde5c7
Mark Dargie <47901212+MarkDargie@users.noreply.github.com>
2022-05-21 11:31:03 +0100

    carousel

--- Frontend/dargiedev/src/app/app.module.ts
+++ Frontend/dargiedev/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import {NgsRevealModule} from 'ngx-scrollreveal';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import {NgsRevealModule} from 'ngx-scrollreveal';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgsRevealModule
+    NgsRevealModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/dargiedev/src/app/home/home.component.ts
+++ Frontend/dargiedev/src/app/home/home.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {NgsRevealConfig} from 'ngx-scrollreveal';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
+export interface PhotosApi {
+  albumId?: number;
+  id?: number;
+  title?: string;
+  url?: string;
+  thumbnailUrl?: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -8,9 +19,57 @@ import {NgsRevealConfig} from 'ngx-scrollreveal';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private reveal: NgsRevealConfig) { }
+  apiData!: any[];
+  limit: number = 10;
+
+  customOptions: OwlOptions = {
+    loop:false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    // center: true,
+    // autoHeight: true,
+    // autoWidth: true,
+    navSpeed: 100,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 2,
+        margin: 500
+      }
+    },
+    nav:true,
+  }
+
+  constructor(
+    private reveal: NgsRevealConfig,
+    private http: HttpClient
+    ) { }
 
   ngOnInit(): void {
+    this.fetch();
+  }
+
+  fetch() {
+    const api = `https://jsonplaceholder.typicode.com/albums/1/photos?_start=0&_limit=${this.limit}`;
+    const http$ = this.http.get<PhotosApi[]>(api);
+
+    http$.subscribe(
+      res => this.apiData = res,
+      err => throwError(err)
+    )
   }
 
 }
